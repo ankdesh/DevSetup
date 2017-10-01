4da076320490fd88bc68d67b26b8fc936282020b
Ankur Deshwal <a.s.deshwal@gmail.com>
2017-10-01 07:05:46 +0530

    Update setup_rc.sh

--- scripts/setup_rc.sh
+++ scripts/setup_rc.sh
@@ -5,6 +5,7 @@ echo "set history save on" >> ~/.gdbinit
 #screen rc
 echo "hardstatus alwayslastline" >> ~/.screenrc
 echo "hardstatus string '%{= kG}[ %{G}%H %{g}][%=%{=kw}%?%-Lw%?%{r}(%{W}%n*%f%t%?(%u)%?%{r})%{w}%?%+Lw%?%?%=%{g}][%{B}%Y-%m-%d %{W}%c %{g}]'" >> ~/.screenrc
+echo "defshell -bash" >> ~/.screenrc
 
 #vimrc 
 echo "set expandtab" >> ~/.vimrc
