4ae68e4e6f4578bc5e130fac926da2fa8aef8122
ankdesh <a.s.deshwal@gmail.com>
2017-10-15 01:29:30 +0530

    Add Dockerfile for Cuda based devel-machine

---
+++ Dockerfiles/cuda-py/Dockerfile.cuda.bash
@@ -0,0 +1,55 @@
+FROM nvidia/cuda:8.0-cudnn6-devel-ubuntu16.04
+
+MAINTAINER Ankur Deshwal <a.s.deshwal@gmail.com>
+
+RUN apt-get update && apt-get install -y sudo
+
+RUN useradd -ms /bin/bash ankdesh
+RUN echo ankdesh:ankdesh@123 | chpasswd
+RUN adduser ankdesh sudo
+
+RUN apt-get update && apt-get install -y \
+        python3-pip \
+        python3-dev \
+    && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN apt-get update && apt-get install -y \
+        python-pip \
+        python-dev \
+    && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+COPY jupyter_notebook_config.py /root/.jupyter/
+
+# Jupyter has issues with being run directly:
+# https://github.com/ipython/ipython/issues/7062
+# We just add a little wrapper script.
+COPY run_jupyter.sh /
+
+# Something Jupyter suggests to do:
+# http://jupyter-notebook.readthedocs.io/en/latest/public_server.html#docker-cmd
+ENV TINI_VERSION v0.9.0
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
+RUN chmod +x /tini
+ENTRYPOINT ["/tini", "--"]
+
+# jupyter
+EXPOSE 8888
+
+USER ankdesh
+WORKDIR /home/ankdesh
+
+RUN pip3 install -U distribute \
+        setuptools \
+        pip \
+        virtualenv
+
+RUN pip install -U distribute \
+        setuptools \
+        pip \
+        virtualenv
+
+CMD ["/bin/bash"]
