307ef3b878f454e06bcef6013a502625b1cb3005
Ankur Deshwal <a.s.deshwal@gmail.com>
2017-10-18 23:17:19 +0530

    Update setup_git.sh

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -1,5 +1,5 @@
 #git config
 git config --global user.email a.s.deshwal@gmail.com
 git config --global user.name ankdesh
-git config --global alias.lol "log --pretty=oneline --abbrev-commit
---graph --decorate"
+git config --global alias.lol "log --pretty=oneline --abbrev-commit --graph --decorate"
+git config --global core.editor "vim"
