4e7f400cb146ea0ccbe361e7e265d5a7f2700de4
Ankur Deshwal <a.s.deshwal@gmail.com>
2017-10-28 21:20:02 +0530

    Update Dockerfile.cuda.bash

--- Dockerfiles/cuda-py/Dockerfile.cuda.bash
+++ Dockerfiles/cuda-py/Dockerfile.cuda.bash
@@ -52,4 +52,6 @@ RUN bash setup_rc.sh
 RUN rm setup_git.sh
 RUN rm setup_rc.sh
 
+RUN echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/nvidia/lib64/:/usr/local/nvidia/lib/:/usr/local/cuda/lib64/" >> ~/.bashrc
+
 CMD ["/bin/bash"]
